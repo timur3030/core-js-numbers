3ea95ebdb0c9a74a4c2280c6afe9f4f0bad67608
Timur <timur87@mail.ru>
2023-12-03 18:36:17 +0300

    getFibonacciNumber and getSumToN

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -254,8 +254,18 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const arr = [0];
+  let first = 0;
+  let second = 1;
+  let next = 0;
+  for (let i = 0; i < index; i += 1) {
+    arr.push(second);
+    next = first + second;
+    first = second;
+    second = next;
+  }
+  return first;
 }
 
 /**
@@ -269,8 +279,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let result = 0;
+  for (let i = 1; i <= n; i += 1) {
+    result += i;
+  }
+  return result;
 }
 
 /**
