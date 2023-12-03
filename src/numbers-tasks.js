1ede0c335b09e7e9bca014fe26497c29168ed105
Timur <timur87@mail.ru>
2023-12-03 05:43:40 +0300

    toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -137,8 +137,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -154,8 +154,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(Math.sqrt(a ** 2 + b ** 2) ** 2 + c ** 2);
 }
 
 /**
@@ -215,8 +215,9 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (typeof +value === 'number' && !Number.isNaN(+value)) return +value;
+  return def;
 }
 
 /**
